ecd8b71ea1d8b8b4395f2666000bbcabe6a15680
Rob Hill <robert.hill0122@gmail.com>
2014-08-12 16:53:47 -0600

    Tasklists are now drop down menus

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function () {
+
+  var tasks = $('.tasks')
+
+  var showPanel = function (id, e) {
+    console.log(e)
+    if (tasks.is(':visible')) {
+      $('.tasks').slideUp('slow')
+    } else {
+      $('.tasks').each(function () {
+        if ($(this).attr('id') == id) {
+          $(this).slideDown('slow');
+          var button = $(this).siblings('dt').find('button');
+        } else {
+          $(this).slideUp('slow');
+        }
+      });
+    }
+  };
+
+  window.showPanel = showPanel;
+
+//  var button = $('.panel button')
+//
+//  button.on('click',function(){
+//    if(button.text == 'Open') {
+//      $(this).text = 'Close';
+//      $(this).siblings.find('.tasks').slideDown('slow')
+//    } else {
+//      $(this).text = 'Open';
+//    }
+//  });
+
+});
+
